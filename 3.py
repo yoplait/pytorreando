a274c872626642591633ba4b099c6ab1e19ce867
Juan Carlos Perez <perezpardojc@gmail.com>
2014-10-12 20:21:59 +0100

    4 lines

---
+++ 3.py
@@ -0,0 +1,13 @@
+MAX =  5
+
+def display_results(results,count=MAX):
+  #if len(results) < count: count = len(results)
+  for i in range(0, count):
+    #print count
+    print results[i],
+
+MAX = 6
+
+data=[1,2,3,4,5,6,7]
+#print MAX
+display_results(data)
