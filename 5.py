c9d349339add478f36dacc9be90ae5a7e0b9cebf
Juan Carlos Perez <perezpardojc@gmail.com>
2014-10-16 20:38:47 +0100

    aprendiendo multi parametros

---
+++ 5.py
@@ -0,0 +1,8 @@
+def display(name, age, spouse,*children):
+  print name, age, spouse, children
+
+
+
+
+
+display('bob',37,'sallu','timmi','Johny','ani')
---
+++ 6.py
@@ -0,0 +1,4 @@
+def display_info(**family):
+  print family
+
+display_info(name='bob', age=3, spouse='sally', child1='timmy', child2='johhny', child3='annie')
