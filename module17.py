530556f212f8b713a41496ce86103863e3bd4065
Juan Carlos Perez <perezpardojc@gmail.com>
2014-10-19 19:08:25 +0100

    lambda try catch

---
+++ module17.py
@@ -0,0 +1,8 @@
+import exceptions
+dir(exceptions)
+
+a = raw_input("Enter a number: ")
+b = raw_input("Enter a number: ")
+
+#print "Division result is : %f" % a / b
+print "Division result is : ", float(a) / float(b)
